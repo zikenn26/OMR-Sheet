c2c446605c700c139e3ff4c5be5a9db463e71d73
zikenn26 <gulnayak1206@gmail.com>
2025-03-04 16:52:40 +0530

    Initial commit

--- server/index.ts
+++ server/index.ts
@@ -58,12 +58,12 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || '127.0.0.1';
+
+server.listen(port, host, () => {
+  log(`Serving on http://${host}:${port}`);
+});
+
 })();
+
